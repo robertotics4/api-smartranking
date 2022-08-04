5e0c72d95cac3950450c54b8f2deb2752a7ebdc0
robertotics4 <robertooliveiragp@gmail.com>
2022-08-04 19:04:09 -0300

    create crud

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,6 +1,6 @@
 import { v1 as uuid } from 'uuid';
 
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { Jogador } from './interfaces/jogador.interface';
 
@@ -10,7 +10,45 @@ export class JogadoresService {
   private readonly logger = new Logger(JogadoresService.name);
 
   async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto): Promise<void> {
-    this.criar(criarJogadorDto);
+    const { email } = criarJogadorDto;
+
+    const jogadorEncontrado = this.jogadores.find(
+      (jogador) => jogador.email === email,
+    );
+
+    if (jogadorEncontrado) {
+      this.atualizar(jogadorEncontrado, criarJogadorDto);
+    } else {
+      this.criar(criarJogadorDto);
+    }
+  }
+
+  async consultarTodosJogadores(): Promise<Jogador[]> {
+    return this.jogadores;
+  }
+
+  async consultarJogadorPeloEmail(email: string): Promise<Jogador> {
+    const jogadorEncontrado = this.jogadores.find(
+      (jogador) => jogador.email === email,
+    );
+
+    if (!jogadorEncontrado) {
+      throw new NotFoundException(`Jogador com e-mail ${email} não encontrado`);
+    }
+
+    return jogadorEncontrado;
+  }
+
+  async deletarJogador(email: string): Promise<void> {
+    const indiceJogadorEncontrado = this.jogadores.findIndex(
+      (jogador) => jogador.email === email,
+    );
+
+    if (!indiceJogadorEncontrado) {
+      throw new NotFoundException(`Jogador com e-mail ${email} não encontrado`);
+    }
+
+    this.jogadores.splice(indiceJogadorEncontrado, 1);
   }
 
   private criar(criarJogadorDto: CriarJogadorDto): void {
@@ -29,5 +67,14 @@ export class JogadoresService {
     this.logger.log(`criarJogadorDto: ${JSON.stringify(jogador, null, 2)}`);
 
     this.jogadores.push(jogador);
+  }
+
+  private atualizar(
+    jogadorEncontrado: Jogador,
+    criarJogadorDto: CriarJogadorDto,
+  ): void {
+    const { nome } = criarJogadorDto;
+
+    jogadorEncontrado.nome = nome;
   }
 }
